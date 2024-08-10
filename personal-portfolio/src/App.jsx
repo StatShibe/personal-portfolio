f37407e7f53cd73824fb081b06cabb4c7a3815f3
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2024-08-10 22:30:24 +0530

    navbar

--- personal-portfolio/src/App.jsx
+++ personal-portfolio/src/App.jsx
@@ -1,7 +1,15 @@
+import NavigationBar from "./components/NavigationBar"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex flex-col h-screen w-screen">
+      <div>
+        <NavigationBar/>
+      </div>
+      <div className="mt-10">
+        <h1>Some content</h1>
+        <p>The standard Lorem Ipsum passage, used since the 1500s</p>
+      </div>
+    </div>
   )
 }
