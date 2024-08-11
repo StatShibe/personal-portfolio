b4fa024d317e017c4e4e7077e9a0e6cacd221e25
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2024-08-11 21:39:56 +0530

    structure

--- personal-portfolio/src/App.jsx
+++ personal-portfolio/src/App.jsx
@@ -7,8 +7,12 @@ export default function App() {
         <NavigationBar/>
       </div>
       <div className="mt-10">
-        <h1>Some content</h1>
-        <p>The standard Lorem Ipsum passage, used since the 1500s</p>
+        <div className="h-[400px] w-screen bg-red-500"></div>
+        <h1 className="text-center">Hi there</h1>
+        <div className="flex flex-row w-screen">
+          <div className = "text-center w-1/2">Jimmy is here</div>
+          <div className="text-center w-1/2">Tommy is here</div>
+        </div>
       </div>
     </div>
   )
