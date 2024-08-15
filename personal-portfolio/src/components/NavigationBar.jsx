7832990c1fa577c198b462e686d2d58cc1638903
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2024-08-15 12:30:58 +0530

    changed

--- personal-portfolio/src/components/NavigationBar.jsx
+++
@@ -1,9 +0,0 @@
-const NavigationBar = () => {
-    return (
-        <div className="w-full h-10 bg-black fixed top-0 left-0 z-50">
-            <h1 className="text-white ml-4">This is the Navigation Bar</h1>
-        </div>
-    );
-}
-
-export default NavigationBar;
