c3261e6660cfd2746e5088942d0603e9793c525f
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2024-08-15 13:18:02 +0530

    vite config

--- web-portfolio/tailwind.config.js
+++ web-portfolio/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+export default {
+ content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     fontFamily: {
       primary: 'Orbitron',
@@ -31,5 +31,5 @@ module.exports = {
     },
   },
   plugins: [],
-};
+}
 
