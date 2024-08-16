937ccc298a650e9f8d5a0bd4d9f0fa99528676c3
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2024-08-16 20:45:45 +0530

    added banner

--- web-portfolio/src/components/Banner.jsx
+++ web-portfolio/src/components/Banner.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 
+import Image from "../assets/avatar.svg";
+
+import { FaGithub, FaYoutube, FaDribbble } from "react-icons/fa";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+
+import { fadeIn } from "../variants";
+
 const Banner = () => {
   return (
-    <div className="section" id="home">
-      Banner
-    </div>
+    <section className="section" id="home">
+      <div className="container mx-auto">
+        <div>
+          <div>
+            <h1>
+              BEN <span>AIDEN</span>
+            </h1>
+          </div>
+          <div>
+            <span>I am a </span>
+            <TypeAnimation
+              sequence={["Developer", 2000, "Designer", 2000, "Youtuber", 2000]}
+            />
+            {/*This is where i stopped last time*/}
+          </div>
+          <div>
+            <img src={Image} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
