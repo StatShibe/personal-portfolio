4d942f59427b75a28a059ce5e8892baf27666d12
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2024-08-17 14:52:39 +0530

    banner styling

--- web-portfolio/src/components/Banner.jsx
+++ web-portfolio/src/components/Banner.jsx
@@ -12,18 +12,43 @@ const Banner = () => {
   return (
     <section className="section" id="home">
       <div className="container mx-auto">
-        <div>
-          <div>
-            <h1>
+        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
+          <div className="flex-1 text-center font-secondary lg:text-left">
+            <h1 className="text-[55px] font-bold leading-[0.8] lg:text-[110px]">
               BEN <span>AIDEN</span>
             </h1>
           </div>
-          <div>
-            <span>I am a </span>
+          <div className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]">
+            <span className="text-white mr-4">I am a</span>
             <TypeAnimation
               sequence={["Developer", 2000, "Designer", 2000, "Youtuber", 2000]}
+              speed={50}
+              className="text-accent"
+              wrapper="span"
+              repeat={Infinity}
             />
-            {/*This is where i stopped last time*/}
+          </div>
+          <p className="mb-8 max-w-lg mx-auto lg:mx-0">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s.
+          </p>
+          <div className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+            <button className="btn btn-lg">Contact me</button>
+            <a href="#" className="text-gradient btn-link">
+              My Portfolio
+            </a>
+          </div>
+          <div className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
+            <a href="#">
+              <FaYoutube />
+            </a>
+            <a href="#">
+              <FaGithub />
+            </a>
+            <a href="#">
+              <FaDribbble />
+            </a>
           </div>
           <div>
             <img src={Image} alt="" />
