34c46b63f4e10c61071fd551e7ad706ca1ea91c7
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2024-08-26 13:47:42 +0530

    fixed sizing issue

--- web-portfolio/src/components/Banner.jsx
+++ web-portfolio/src/components/Banner.jsx
@@ -9,60 +9,61 @@ import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
 
 const Banner = () => {
-  return (
-    <section className="min-h-[85vh] lg:min-h-[78vh] flex items-center" id="home">
-      <div className="container mx-auto">
-        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
-          {/* Text */}
-          <div className="flex-1 text-center font-secondary lg:text-left">
-            <h1 className="text-[55px] font-bold leading-[0.8] lg:text-[110px]">
-              BEN <span>AIDEN</span>
-            </h1>
-            <div className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]">
-              <span className="text-white mr-4">I am a</span>
-              <TypeAnimation sequence={[
-                'Developer',
-                2000,
-                'Designer',
-                2000,
-                'Youtuber',
-                2000
-              ]}
-                speed={50}
-                className="text-accent"
-                wrapper="span"
-                repeat={Infinity}
-              />
-            </div>
-          </div>
-              <p className="mb-8 max-w-lg mx-auto lg:mx-0">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-              <div className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
-                <button className="btn btn-lg">Contact Me</button>
-                <a href="#" className="text-gradient btn-link">
-                  My Portfolio
-                </a>
-              </div>
-              {/* Socials */}
-              <div className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
-                <a href="#">
-                  <FaYoutube/>
-                </a>
-                <a href="#">
-                  <FaGithub/>
-                </a>
-                <a href="#">
-                  <FaDribbble/>
-                </a>
-              </div>
-          <div className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]">
-            <img src={Image} alt="Profile Image"></img>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className="min-h-[85vh] lg:min-h-[78vh] flex items-center" id="home">
+			<div className="container mx-auto">
+				<div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
+					{/* Text */}
+					<div className="flex-1 text-center font-secondary lg:text-left">
+						<motion.h1 variants={fadeIn('up',0.4)} initial = "hidden" whileInView={'show'} viewport={{once: false, amount: 0.7}}className="text-[55px] font-bold leading-[0.8] lg:text-[110px]">
+							BEN <span>AIDEN</span>
+						</motion.h1>
+						<motion.div variants={fadeIn('up',0.7)} initial = "hidden" whileInView={'show'} viewport={{once: false, amount: 0.7}}   className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]">
+							<span className="text-white mr-4">I am a</span>
+							<TypeAnimation sequence={[
+								'Developer',
+								2000,
+								'Designer',
+								2000,
+								'Youtuber',
+								2000
+							]}
+								speed={50}
+								className="text-accent"
+								wrapper="span"
+								repeat={Infinity}
+							/>
+						</motion.div>
+						<p className="mb-8 max-w-lg mx-auto lg:mx-0">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+							</p>
+							<div className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+								<button className="btn btn-lg">Contact Me</button>
+								<a href="#" className="text-gradient btn-link">
+									My Portfolio
+								</a>
+							</div>
+							{/* Socials */}
+							<div className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
+								<a href="#">
+									<FaYoutube/>
+								</a>
+								<a href="#">
+									<FaGithub/>
+								</a>
+								<a href="#">
+									<FaDribbble/>
+								</a>
+							</div>
+					</div>
+							
+					<div className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]">
+						<img src={Image} alt="Profile Image"></img>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Banner;
