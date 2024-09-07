397909eb2f2d5de324275c738cc832085f44fc60
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2024-09-07 20:07:47 +0530

    added about section

--- web-portfolio/src/components/Banner.jsx
+++ web-portfolio/src/components/Banner.jsx
@@ -15,10 +15,10 @@ const Banner = () => {
 				<div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
 					{/* Text */}
 					<div className="flex-1 text-center font-secondary lg:text-left">
-						<motion.h1 variants={fadeIn('up',0.4)} initial = "hidden" whileInView={'show'} viewport={{once: false, amount: 0.7}}className="text-[55px] font-bold leading-[0.8] lg:text-[110px]">
+						<motion.h1 variants={fadeIn('up',0.3)} initial = "hidden" whileInView={'show'} viewport={{once: false, amount: 0.7}}className="text-[55px] font-bold leading-[0.8] lg:text-[110px]">
 							BEN <span>AIDEN</span>
 						</motion.h1>
-						<motion.div variants={fadeIn('up',0.7)} initial = "hidden" whileInView={'show'} viewport={{once: false, amount: 0.7}}   className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]">
+						<motion.div variants={fadeIn('up',0.4)} initial = "hidden" whileInView={'show'} viewport={{once: false, amount: 0.7}}   className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]">
 							<span className="text-white mr-4">I am a</span>
 							<TypeAnimation sequence={[
 								'Developer',
@@ -34,17 +34,17 @@ const Banner = () => {
 								repeat={Infinity}
 							/>
 						</motion.div>
-						<p className="mb-8 max-w-lg mx-auto lg:mx-0">
+						<motion.p variants={fadeIn('up',0.5)} initial = "hidden" whileInView={'show'} viewport={{once: false, amount: 0.7}} className="mb-8 max-w-lg mx-auto lg:mx-0" >
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-							</p>
-							<div className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
+							</motion.p>
+							<motion.div variants={fadeIn('up',0.6)} initial = "hidden" whileInView={'show'} viewport={{once: false, amount: 0.7}} className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
 								<button className="btn btn-lg">Contact Me</button>
 								<a href="#" className="text-gradient btn-link">
 									My Portfolio
 								</a>
-							</div>
+							</motion.div>
 							{/* Socials */}
-							<div className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
+							<motion.div variants={fadeIn('up',0.5)} initial = "hidden" whileInView={'show'} viewport={{once: false, amount: 0.7}} className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
 								<a href="#">
 									<FaYoutube/>
 								</a>
@@ -54,12 +54,12 @@ const Banner = () => {
 								<a href="#">
 									<FaDribbble/>
 								</a>
-							</div>
+							</motion.div>
 					</div>
 							
-					<div className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]">
+					<motion.div variants={fadeIn('down',0.5)} initial = "hidden" whileInView={'show'} className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]">
 						<img src={Image} alt="Profile Image"></img>
-					</div>
+					</motion.div>
 				</div>
 			</div>
 		</section>
