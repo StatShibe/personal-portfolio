9b2fecc8c51778f05020a55b9ef0131a5d83c452
Haresh Moorthy <100016407+StatShibe@users.noreply.github.com>
2024-12-21 20:10:03 +0530

    testing workflow

--- web-portfolio/src/components/Banner.jsx
+++ web-portfolio/src/components/Banner.jsx
@@ -35,7 +35,7 @@ const Banner = () => {
 							/>
 						</motion.div>
 						<motion.p variants={fadeIn('up',0.5)} initial = "hidden" whileInView={'show'} viewport={{once: false, amount: 0.7}} className="mb-8 max-w-lg mx-auto lg:mx-0" >
-							An Idiot admires complexity, a genius admires simplicity.
+							An Idiot admires complexity, a genius admires simplicity
 							</motion.p>
 							<motion.div variants={fadeIn('up',0.6)} initial = "hidden" whileInView={'show'} viewport={{once: false, amount: 0.7}} className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
 								<button className="btn btn-lg">Contact Me</button>
